0ce27167137b8f436a372ad0cab75e03216df714
Aparnagopal <gopal_aparna@yahoo.com>
2025-08-28 18:59:07 +0530

    Update route.ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,8 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  return new Response(JSON.stringify({ message: "Hello from Next.js API!" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+export async function GET() {
+  return NextResponse.json({ message: "Hello from API!" });
 }
